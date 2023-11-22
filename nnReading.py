0b482458f869d488a595ed5f1dda3424900a67b5
Mihail-Pustobaev <89999504+Mihail-Pustobaev@users.noreply.github.com>
2023-11-22 18:54:17 +0300

    added comments nnReading.py

--- nnReading.py
+++ nnReading.py
@@ -4,17 +4,18 @@ import pytesseract
 from PIL import Image
 import aspose.words as aw
 
+# функция, в которую передаётся путь до картинки, текст с которой нужно прочитать
 def readFile(path):
-    pytesseract.pytesseract.tesseract_cmd = 'Tesseract-OCR\\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = 'Tesseract-OCR\\tesseract.exe' # ключевая строчка, код работает с помощью библиотеки Tesseract, для работы нужно установить Tesseract
 
-    image = Image.open(path)
+    image = Image.open(path) # открывается картинка
 
     try:
         text = pytesseract.image_to_string(image, lang='rus')
-    except Exception as e:
+    except Exception as e: # если произошло исключение, возращаем False и текст ошибки
         return False, e
 
-    image_name = path.split('/')[-1].split('.')[0]
+    image_name = path.split('/')[-1].split('.')[0] # разделенние пути до картинки по слешам, нужно чтобы получить имя файла картинки
 
     doc = aw.Document()
 
@@ -23,7 +24,7 @@ def readFile(path):
     builder.write(text)
 
     try:
-        doc.save(f"outImgText\\{image_name}.docx")
+        doc.save(f"outImgText\\{image_name}.docx") # сохранение
     except Exception as e:
         return False, e
 
